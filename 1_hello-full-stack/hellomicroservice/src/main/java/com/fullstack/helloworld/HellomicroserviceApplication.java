3321a68899e1653b8c9f082a0cd180a36c7d4928
AshwinMyJobs <myjobsash@gmail.com>
2024-02-12 13:10:19 +0530

    Hello world microservice code push

---
+++ 1_hello-full-stack/hellomicroservice/src/main/java/com/fullstack/helloworld/HellomicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.fullstack.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellomicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellomicroserviceApplication.class, args);
+	}
+
+}
