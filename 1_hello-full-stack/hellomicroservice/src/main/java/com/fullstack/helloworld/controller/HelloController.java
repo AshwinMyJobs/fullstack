11162cc025440783258113d76df06d0c34c267d8
myjobsash <myjobsash@gmail.com>
2024-02-15 14:35:18 +0530

    Uodate controller to return object code push

--- 1_hello-full-stack/hellomicroservice/src/main/java/com/fullstack/helloworld/controller/HelloController.java
+++ 1_hello-full-stack/hellomicroservice/src/main/java/com/fullstack/helloworld/controller/HelloController.java
@@ -1,17 +1,27 @@
 package com.fullstack.helloworld.controller;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.fullstack.helloworld.dto.Hello;
 
 @RestController
 @RequestMapping("hello")
 public class HelloController {
 
 	@GetMapping("{name}")
-	public String sayHello(@PathVariable String name ) {
-		return "Welcome to full stack programming " + name +" , Happy learning!!!!!";
+	public @ResponseBody Hello sayHello(@PathVariable String name ) {
+		System.out.println("I am called.........");
+		Hello hello = new Hello();
+		hello.setMessage("Welcome to full stack programming " + name +" , Happy learning!!!!!");
+		//return "Welcome to full stack programming " + name +" , Happy learning!!!!!";
+		return hello;
 	}
-	
 }
