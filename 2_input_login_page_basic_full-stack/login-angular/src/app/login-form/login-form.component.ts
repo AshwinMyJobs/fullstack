9525dd82b7cdfa534f9cf6a0601cd944dacef6bf
myjobsash <myjobsash@gmail.com>
2024-03-26 11:02:03 +0530

    Update angular project and deleted project which was not needed

--- 2_input_login_page_basic_full-stack/login-angular/src/app/login-form/login-form.component.ts
+++ 2_input_login_page_basic_full-stack/login-angular/src/app/login-form/login-form.component.ts
@@ -1,15 +1,29 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { User } from './login-model';
 
 @Component({
   selector: 'app-login-form',
-  templateUrl: './login-form.component.html',
+  templateUrl: './login-form.view.html',
   styleUrl: './login-form.component.css'
 })
+
 export class LoginFormComponent {
+
+  user : User = new User();
+
+  season :String = "Summer";
+
 
   onSubmit(loginForm: NgForm) {
     console.log(loginForm.value);
   }
   
+
+  onClick() {
+    console.log("Click method is called");
+    console.log(this.user.userName);
+    console.log(this.user.passWord);
+    console.log(this.season);
+  }
 }
